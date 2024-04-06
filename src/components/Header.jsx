466439cbb85bfba62c5f42ec4952a7784ac58b40
Saleemullah Khan Saleem <142225115+saleemullahkhansaleem@users.noreply.github.com>
2024-04-06 11:36:49 +0500

    fix
    images convert to wepp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link, NavLink, useLocation } from "react-router-dom";
 import { CiDark, CiLight } from "react-icons/ci";
+import { IoMdMenu } from "react-icons/io";
 
 const Header = ({ setmode, mode }) => {
-  const param = useLocation();
-  //   console.log(param);
+  const [showMenu, setShowMenu] = useState(false);
+
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuRef]);
+
   const navLinks = [
     {
       name: "Home",
@@ -25,20 +42,32 @@ const Header = ({ setmode, mode }) => {
   ];
 
   return (
-    <nav className="w-full flex items-center justify-between gap-2 bg-zinc-200 dark:bg-zinc-700 px-8">
+    <nav className="w-full flex flex-wrap items-center justify-between gap-2 bg-zinc-200 dark:bg-zinc-700 px-2 md:px-4 relative">
+      <div
+        className="block md:hidden"
+        onClick={() => setShowMenu((prv) => !prv)}
+      >
+        <IoMdMenu />
+      </div>
       <Link to="/" className="uppercase text-xl text-zinc-500">
         Saleem Projects
       </Link>
-      <div className="flex items-center">
+      <div
+        ref={menuRef}
+        className={`absolute md:static top-8 flex-col py-2 md:py-0 ${
+          showMenu ? "flex" : "hidden"
+        } md:flex md:flex-row items-center bg-zinc-700 z-10 shadow`}
+      >
         {navLinks.map((link, i) => (
           <NavLink
             key={i}
             className={({ isActive }) =>
               isActive
-                ? "bg-zinc-100 dark:bg-zinc-800"
-                : "bg-zinc-200 dark:bg-zinc-700 "
+                ? "bg-zinc-100 dark:bg-zinc-800 w-full md:w-auto"
+                : "bg-zinc-200 dark:bg-zinc-700 w-full md:w-auto"
             }
             to={link.url}
+            onClick={() => setShowMenu(false)}
           >
             <div className="px-4 py-2 text-sm">{link.name}</div>
           </NavLink>
